65c0df255dc6b282a847e9854ff0de8911d4405b
Vitorvgc <vitorvgc07@gmail.com>
2016-09-17 23:09:19 -0300

    Create Memory class

---
+++ src/sample/Memory.java
@@ -0,0 +1,39 @@
+package sample;
+
+/**
+ * Created by Vítor on 17/09/2016.
+ */
+public class Memory {
+
+    private int totalMemory;
+    private int soMemory;
+    private int memory;
+
+    public Memory() {
+
+    }
+
+    public void allocateProcess(Process process) {
+
+    }
+
+    public void desallocateProcess(Process process) {
+
+    }
+
+    private void allocateProcessWithFirstFit(Process p) {
+
+    }
+
+    private void allocateProcessWithBestFit(Process p) {
+
+    }
+
+    private void allocateProcessWithWorsttFit(Process p) {
+
+    }
+
+    private void allocateProcessWithNextFit(Process p) {
+
+    }
+}
