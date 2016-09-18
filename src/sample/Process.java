d45db020b3881e95df66f70a14d18e990265b376
Vitorvgc <vitorvgc07@gmail.com>
2016-09-17 22:02:36 -0300

    Add Process random constructor

--- src/sample/Process.java
+++ src/sample/Process.java
@@ -1,16 +1,26 @@
 package sample;
 
+import java.util.Random;
+
 /**
  * Created by Vítor on 17/09/2016.
  */
 public class Process {
 
-    int creationTime;
-    int duration;
-    int memory;
+    private int creationTime;
+    private int duration;
+    private int memory;
 
-    static Process newProcess() {
-        
+    public Process(int minCreationTime, int maxCreationTime, int minDuration, int maxDuration, int minMemory, int maxMemory) {
+
+        Random random = new Random();
+
+        this.creationTime =  random.nextInt(maxCreationTime - minCreationTime) + minCreationTime;
+        this.duration = random.nextInt(maxDuration - minDuration) + minDuration;
+        this.memory = random.nextInt(maxMemory - minMemory) + minMemory;
+
+        if(this.creationTime < 0 || this.duration < 0 || this.memory < 0)
+            System.out.println("WARNING: Process with negative values");
     }
 
 }
