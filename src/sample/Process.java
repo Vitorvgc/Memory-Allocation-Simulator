393ac9c167ce4f180a3945f1867fc1e03e14697d
Vitorvgc <vitorvgc07@gmail.com>
2016-09-17 21:44:32 -0300

    Create Process class

---
+++ src/sample/Process.java
@@ -0,0 +1,16 @@
+package sample;
+
+/**
+ * Created by Vítor on 17/09/2016.
+ */
+public class Process {
+
+    int creationTime;
+    int duration;
+    int memory;
+
+    static Process newProcess() {
+        
+    }
+
+}
