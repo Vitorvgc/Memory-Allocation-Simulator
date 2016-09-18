2f3df52edaa2eac2fef07c9b523da471c935f9b1
Vitorvgc <vitorvgc07@gmail.com>
2016-09-18 00:21:56 -0300

    Add memory allocation test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,14 @@
 package sample;
 
+import Enums.AllocationType;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Main extends Application {
 
@@ -14,8 +18,19 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-    }
 
+        Memory memory = new Memory(100, new Process(20));
+        memory.printMemory();
+
+        memory.allocateProcess(new Process(30), AllocationType.FIRST_FIT);
+        memory.printMemory();
+
+        memory.allocateProcess(new Process(20), AllocationType.FIRST_FIT);
+        memory.printMemory();
+
+        memory.allocateProcess(new Process(40), AllocationType.FIRST_FIT);
+        memory.printMemory();
+    }
 
     public static void main(String[] args) {
         launch(args);
