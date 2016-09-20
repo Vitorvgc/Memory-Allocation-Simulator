8c68472be319100b97a3b5ce10809a180c3649a5
Vitorvgc <vitorvgc07@gmail.com>
2016-09-20 18:04:29 -0300

    Add SystemManager allocation method

--- src/sample/SystemManager.java
+++ src/sample/SystemManager.java
@@ -4,35 +4,52 @@ import Enums.AllocationType;
 
 import java.util.ArrayList;
 import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by vitorchagas on 20/09/16.
  */
 public class SystemManager {
 
-    private int processesNumber;
+
     private Memory memory;
     private AllocationType allocationType;
-
     private ArrayList<Process> processes;
     private ArrayList<Process> processesQueue;
-
     private Timer timer;
 
     public SystemManager(ArrayList<Process> processes, AllocationType allocationType, int totalMemory, Process so) {
         this.processes = processes;
-        this.processesNumber = processes.size();
         this.allocationType = allocationType;
         this.memory = new Memory(totalMemory, so);
         this.processesQueue = new ArrayList<>();
         this.timer = new Timer();
     }
 
-    public void allocateProcess(Process process) {
+    public void start() {
 
     }
 
-    public void desallocateProcess(Process process) {
+    private void allocateProcess(Process process) {
+
+        boolean allocationSucessful = this.memory.allocateProcess(process, this.allocationType);
+
+        if(allocationSucessful) {
+
+            TimerTask timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    desallocateProcess(process);
+                }
+            };
+
+            this.timer.schedule(timerTask, process.getDuration());
+
+        }
+
+    }
+
+    private void desallocateProcess(Process process) {
 
     }
 
